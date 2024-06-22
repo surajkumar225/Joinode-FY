dd6a40785bccc044f3d23bea7348b15509d06fda
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2024-06-22 21:37:55 +0530

    Delete client directory

--- client/src/pages/NotFound.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function NotFound() {
-  return (
-    <div className="w-full h-[calc(100dvh-60px)] bg-gray-800 text-white flex justify-center items-center">
-      Not Found
-    </div>
-  )
-}
